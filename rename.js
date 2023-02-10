817c691f1b8378838809dff10943167790f428a8
hayabusa <s.hashimoto@hs-sh.net>
2023-02-10 11:29:44 +0900

    rename to NotoEmoji format

--- rename.js
+++ rename.js
@@ -1,31 +1,13 @@
 const fs = require('fs');
 
-(async () => {
-    const emojilist = await fetch("https://raw.githubusercontent.com/misskey-dev/misskey/ad6844ac4a5c0b84917c171f401178941e0af552/packages/frontend/src/emojilist.json").then(r => r.json());
+const lines = fs.readFileSync("emoji-test.txt").toString().split("\n").filter(e => { return !e.startsWith("#") }).filter(e => e != "")
 
-    emojilist.forEach(e => {
-        let res = "";
-        for (const c of e.char) {
-            const code = c.codePointAt(0);
-            if (code < 0xD800 || code > 0xDFFF) {
-                if (res != "") res += "-"
-                res += code.toString(16)
-            }
-        }
-        console.log(res);
-
-        var src = 'svg/emoji_u' + res.replaceAll("-", "_") + '.svg';
-        if (!fs.existsSync(src))
-            src = 'svg/' + e.name.replaceAll("_", " ") + '.svg';
-
-        if (fs.existsSync(src)) {
-            try {
-                fs.rename(src, "svg/" + res + ".svg", () => { })
-                console.log(e.char, res + ".svg", src);
-            }
-            catch (e) {
-                console.error(e);
-            }
-        }
-    })
-})();
+for (const line of lines) {
+    const row = line.split(/[#;]/).map(e => e.trim().toLowerCase().replace(/.*e\d+\.\d+/, "").trim())
+    const original = row[2].replaceAll(":", " ").replaceAll(",", " ").replaceAll("  ", " ") + ".svg";
+    const result = "emoji_u" + row[0].replaceAll(" ", "_") + ".svg";
+    if (fs.existsSync("svg/" + original)) {
+        fs.renameSync("svg/" + original, "svg/" + result);
+        console.log(original, result);
+    }
+}
